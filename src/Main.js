1cd44e1595d78d43fd5d09c8d84153864afa7038
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2024-01-26 14:33:16 +0530

    calulator

---
+++ src/Main.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './index.css';
+
+function Main() {
+  const [inputvalue, setinputvalue] = useState('');
+
+  function display(value) {
+    setinputvalue(inputvalue + value);
+  }
+
+  function calculate() {
+    var answers = eval(inputvalue);
+    setinputvalue(answers);
+  }
+
+  function clear() {
+    setinputvalue('');
+  }
+
+  return (
+    <div>
+      <h1>calculator</h1>
+      <form class="calculator" name="calc">
+        <input type="text" class="value" value={inputvalue} />
+        <span class="num clear" onClick={() => clear()}>
+          clear
+        </span>
+        <span onClick={() => display('/')}>/</span>
+        <span onClick={() => display('*')}>*</span>
+        <span onClick={() => display('7')}>7</span>
+        <span onClick={() => display('8')}>8</span>
+        <span onClick={() => display('9')}>9</span>
+        <span onClick={() => display('-')}>-</span>
+        <span onClick={() => display('4')}>4</span>
+        <span onClick={() => display('5')}>5</span>
+        <span onClick={() => display('6')}>6</span>
+        <span className="plus" onClick={() => display('+')}>
+          +
+        </span>
+        <span onClick={() => display('1')}>1</span>
+        <span onClick={() => display('2')}>2</span>
+        <span onClick={() => display('3')}>3</span>
+        <span onClick={() => display('0')}>0</span>
+        <span onClick={() => display('00')}>00</span>
+        <span onClick={() => display('.')}>.</span>
+        <span class="num equal" onClick={() => calculate()}>
+          =
+        </span>
+      </form>
+    </div>
+  );
+}
+
+export default Main;
